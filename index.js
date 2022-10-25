491c7eb19638a829b9a3a41ef07639a15ded77c6
Bernard Legaspi <blegaspi26@gmail.com>
2022-10-25 15:18:10 -0400

    new changes

--- index.js
+++ index.js
@@ -1,43 +1,72 @@
 const url = "http://localhost:3000/Songs"
 const imgHolder = document.querySelector("#music-menu")
 
-function loadIn (){
-fetch(url)
-.then (res => res.json())
-.then (data => {
-    data.forEach(elem => {
-        loadImage(elem)
-    })
-})
+function loadIn() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(elem => {
+                loadImage(elem)
+            })
+        })
 }
-
 
 function loadImage(elem) {
     const image = document.createElement('img')
     imgHolder.append(image)
-    image.src = elem.img 
+    image.src = elem.img
     image.addEventListener('click', () => {
         createInfo(elem)
     })
+
+    const about = document.querySelector(".about");
+    about.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus adipisci nemo rem asperiores doloribus amet dolor facere quidem esse. Commodi, vero qui earum officia maiores voluptates expedita minus adipisci. Mollitia.yup"
+
 }
 
-function createInfo(elem){
-    const name = document.querySelector('#name')
-    const artist = document.querySelector('h3.artist')
-    const vid = document.querySelector('#video')
-    name.textContent = elem.song
-    artist.textContent = elem.artist
-    vid.src = elem.url
+function createInfo(elem) {
+
+    const songList = document.querySelector("#song-list")
+
+    const songBox = document.createElement("li")
+    songBox.id = "song"
+
+    const songTitle = document.createElement("h3")
+    const songCover = document.createElement("img")
+    const songArtist = document.createElement("h4")
+    const songUrl = document.createElement("p")
+    const reviewList = document.createElement("ul")
+
+    const reviewForm = document.createElement("form")
+    const formLabel = document.createElement("label")
+    const formInput = document.createElement("input")
+    const formH5 = document.createElement("h5")
+    const submitReview = document.createElement("button")
+
+    formH5.textContent = "Add a Song Review!"
+    reviewForm.id = "review-form"
+    formLabel.textContent = "Review: "
+    formInput.id = "add-review"
+    formInput.type = "submit"
+    submitReview.textContent = " SUBMIT "
+
+    reviewForm.addEventListener("submit", (e) => {
+
+        e.preventDefault
+
+        const newReview = document.createElement("li")
+        newReview.textContent = e.target["add-review"].value
+        reviewList.append(newReview)
+    })
+
+    songTitle.textContent = elem.song
+    songCover.src = elem.img
+    songArtist.textContent = elem.artist
+    songUrl.textContent = elem.url
+
+    songList.append(songBox)
+    songBox.append(songTitle, songCover, songArtist, songUrl, reviewForm)
+    reviewForm.append(formH5, formLabel, formInput, reviewList, submitReview)
 }
-
-
-
-
-
-
-
-
-
-
 
 loadIn()
