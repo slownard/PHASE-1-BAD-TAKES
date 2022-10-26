f5e314d6cbd66173f8c11cc2af9d4340913c8b64
Bernard Legaspi <blegaspi26@gmail.com>
2022-10-26 16:58:22 -0400

    styling

--- index.js
+++ index.js
@@ -4,23 +4,41 @@ const newSong = document.querySelector("#new-music");
 const papaR = [];
 const patchR = document.querySelector("#ez");
 const nameSearch = document.querySelector("#new-rating");
+const songList = document.querySelector("#song-list")
+const songBox = document.createElement("li")
+songBox.id = "song"
+const songTitle = document.createElement("h3")
+const songCover = document.createElement("img")
+const songArtist = document.createElement("h4")
+const songUrl = document.createElement("p")
+const reviewList = document.createElement("ul")
+const reviewForm = document.createElement("form")
+const formLabel = document.createElement("label")
+const formInput = document.createElement("input")
+const formH5 = document.createElement("h5")
+const submitReview = document.createElement("button")
+submitReview.id = "submitReview"
+
 
 function loadIn() {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((elem) => {
-        loadImage(elem);
-      });
-      getBread(data);
-    });
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((elem) => {
+                loadImage(elem);
+            });
+            getBread(data);
+        });
 }
 
 
+//loading images in top bar 
 function loadImage(elem) {
 
     const about = document.querySelector(".about");
-    about.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus adipisci nemo rem asperiores doloribus amet dolor facere quidem esse. Commodi, vero qui earum officia maiores voluptates expedita minus adipisci. Mollitia.yup"
+    about.textContent = "WE ARE HERE TO PROVIDE A UNIQUE EXPERIENCE TO MUSIC EXPERTS AND TO THE AVERAGE MELOMANIAC. JOIN OUR COMMUNITY IN REDEFINING HOW WE VIEW MUSIC. IN THIS SPACE WE ARE ABLE TO INTRODUCE EACH OTHER TO NEW LENSES. ALL GENRES.ALL ARTISTS.ONE COMMUNITY"
+
+
 
     const image = document.createElement('img')
     imgHolder.append(image)
@@ -28,27 +46,7 @@ function loadImage(elem) {
     image.addEventListener('click', () => {
         createInfo(elem)
     })
-
-    // const about = document.querySelector(".about");
-    // about.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus adipisci nemo rem asperiores doloribus amet dolor facere quidem esse. Commodi, vero qui earum officia maiores voluptates expedita minus adipisci. Mollitia.yup"
-
 }
-const songList = document.querySelector("#song-list")
-
-const songBox = document.createElement("li")
-songBox.id = "song"
-
-const songTitle = document.createElement("h3")
-const songCover = document.createElement("img")
-const songArtist = document.createElement("h4")
-const songUrl = document.createElement("p")
-const reviewList = document.createElement("ul")
-
-const reviewForm = document.createElement("form")
-const formLabel = document.createElement("label")
-const formInput = document.createElement("input")
-const formH5 = document.createElement("h5")
-const submitReview = document.createElement("button")
 
 function createInfo(elem) {
 
@@ -74,8 +72,8 @@ function createInfo(elem) {
     songUrl.textContent = elem.url
 
     songList.append(songBox)
-    songBox.append(songTitle, songCover, songArtist, songUrl, reviewForm)
-    reviewForm.append(formH5, formLabel, formInput, reviewList, submitReview)
+    songBox.append(songTitle, songCover, songArtist, songUrl, reviewList, reviewForm)
+    reviewForm.append(formH5, formLabel, formInput, submitReview)
 }
 
 loadIn()
